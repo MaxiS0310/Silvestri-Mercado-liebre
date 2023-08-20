bfbeb0accb51c34c72e8612ef62a3c0a0bd37eb2
MaxiS0310 <ernestomaximilianosilvestri@gmail.com>
2023-08-20 17:46:47 -0300

    server and header created

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+app.use(express.static(path.resolve(__dirname, "./public")));
 
 app.listen(3000, () => {
     console.log("Server Run")
+});
+
+app.get("/", function(req,res){
+    res.sendFile(path.resolve(__dirname, "views/home.html"))
 });
